453c08cc9ccd1d8d895fe908fee6f940eca0999e
luisgiltajamar <luis.gil@tajamar365.com>
2014-11-06 10:23:22 +0100

    a

---
+++ WebApiCursos/Repositorio/IRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiCursos.Repositorio
+{
+    interface IRepositorio<TEntidad>
+    {
+        int Add(TEntidad modelo);
+        int Borrar(TEntidad modelo);
+        int Borrar(Expression<Func<TEntidad, bool>> lam);
+        int Actualizar(TEntidad modelo);
+        List<TEntidad> Get();
+        List<TEntidad> Get(Expression<Func<TEntidad, bool>> lam);
+        TEntidad Get(int pk);
+
+
+    }
+}
---
+++ WebApiCursos/Repositorio/Repositorio.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using WebApiCursos.Models;
+
+
+namespace WebApiCursos.Repositorio
+{
+    public class Repositorio<TEntidad> : IRepositorio<TEntidad> where TEntidad : class 
+    {
+
+        protected cursoEntities Context;
+
+        public Repositorio(cursoEntities context)
+        {
+            Context = context;
+        }
+
+        protected DbSet<TEntidad> DbSet
+        {
+            get { return Context.Set<TEntidad>(); }
+
+
+        }
+
+        public int Add(TEntidad modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Borrar(TEntidad modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Borrar(Expression<Func<TEntidad, bool>> lam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Actualizar(TEntidad modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntidad> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntidad> Get(Expression<Func<TEntidad, bool>> lam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntidad Get(int pk)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
